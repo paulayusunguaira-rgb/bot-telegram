52059aa34f458b28e2ccb391434596501d543302
paulayusunguaira-rgb <paulayusunguaira@gmail.com>
2026-09-24 09:31:06 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
 
 import os
 import random
